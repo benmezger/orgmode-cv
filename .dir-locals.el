9103ee7e4721d6ffef99275273b5422cece1f285
Ben Mezger <me@benmezger.nl>
2026-02-21 17:27:31 +0100

    Improve layout and emacs scripts

--- .dir-locals.el
+++ .dir-locals.el
@@ -11,16 +11,32 @@
                           (setq-local org-export-async-init-file
                                       (expand-file-name "cv-export-init.el" cv-dir))
 
+                          ;; Define export function for easy calling
+                          (defun benmezger/cv-export-pdfs ()
+                            "Export CV org files to PDF."
+                            (interactive)
+                            (let ((name (file-name-nondirectory buffer-file-name))
+                                  (dir (file-name-directory buffer-file-name)))
+                              (cond
+                               ((member name '("cv.org"))
+                                (org-latex-export-to-pdf t))
+                               ((string= name "basecv.org")
+                                (dolist (f '("cv.org"))
+                                  (with-current-buffer (find-file-noselect (expand-file-name f dir))
+                                    (org-latex-export-to-pdf t)))))))
+
                           ;; Auto-export to PDF on save
-                          (add-hook 'after-save-hook
-                                    (lambda ()
-                                      (let ((name (file-name-nondirectory buffer-file-name))
-                                            (dir (file-name-directory buffer-file-name)))
-                                        (cond
-                                         ((member name '("cv.org"))
-                                          (org-latex-export-to-pdf t))
-                                         ((string= name "basecv.org")
+                          (add-hook 'after-save-hook #'benmezger/cv-export-pdfs nil t))))))
+ (latex-mode . ((eval . (progn
+                          (let ((cv-dir (file-name-directory
+                                         (or (buffer-file-name) default-directory))))
+                            ;; Auto-export CVs when custom.cls changes
+                            (add-hook 'after-save-hook
+                                      (lambda ()
+                                        (when (string= (file-name-nondirectory buffer-file-name) "custom.cls")
                                           (dolist (f '("cv.org"))
-                                            (with-current-buffer (find-file-noselect (expand-file-name f dir))
-                                              (org-latex-export-to-pdf t)))))))
-                                    nil t)))))))
+                                            (let ((org-file (expand-file-name f cv-dir)))
+                                              (when (file-exists-p org-file)
+                                                (with-current-buffer (find-file-noselect org-file)
+                                                  (org-latex-export-to-pdf t)))))))
+                                      nil t)))))))
